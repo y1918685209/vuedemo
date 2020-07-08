b8a723bbe2b80bed84be4f53a655c8929d3c7ed1
Tommy-Shelby-y <1918685209@qq.com>
2020-07-09 07:59:45 +0800

    添加公共的navbar文件

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="navbar">
+    <div  class="navbar">
         <div class="left"><slot name="left">1</slot></div>
         <div class="center"><slot name="center">2</slot></div>
         <div class="right"><slot name="right">3</slot></div>
