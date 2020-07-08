c111788173063685bc43bc9d0f5abb1cf2ed5fe5
Tommy-Shelby-y <1918685209@qq.com>
2020-07-08 08:11:28 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue- router'
 Vue.use(Router)
-const Home = () => import("../views /home/Home")
+const Home = () => import("../views/home/Home")
 const Category = () => import("../views/ category/Category" )
 const Cart = () => import("../views /cart/Cart")
-const Proflie = () => import(". ./views/ profile/Profile" )
+const Profile = () => import(". ./views/ profile/Profile" )
 
 export default new Router({
   routes:[
@@ -15,6 +15,18 @@ export default new Router({
     {
       path:"/home",//首页
       component:Home
+    },
+    {
+      path:"/category",//分类
+      component:Category
+    },
+    {
+      path:"/cart",//购物车
+      component:Cart
+    },
+    {
+      path:"/profile",//我的
+      component:Profile
     }
   ]
 })
